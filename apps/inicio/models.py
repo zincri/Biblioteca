4cbcf90fe230e6fda85f990d91466dad88339dd9
Jhoana13 <jhoanadominguez97@gmail.com>
2019-12-12 02:09:05 -0600

    jhoana
    relaciones change

--- apps/inicio/models.py
+++ apps/inicio/models.py
@@ -2,18 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Prestador(User):
-    telefono = models.CharField(max_length=15,blank=True,null=True)
-    activo = models.BooleanField(default=True,blank=True,null=True)
-    def __str__(self):
-        return str(self.id)+" - "+self.username
-    def name(self):
-        return str(self.username)
 class Autor(User):
     telefono = models.CharField(max_length=15,blank=True,null=True)
     activo = models.BooleanField(default=True,blank=True,null=True)
     def __str__(self):
         return str(self.id)+" - "+self.username
+
     def name(self):
         return str(self.username)
 
@@ -27,12 +21,23 @@ class Libro(models.Model):
     anio_publicacion = models.CharField(max_length=10,blank=True,null=True)
     estado_prestado = models.BooleanField(default=False,blank=True,null=True)
     activo = models.BooleanField(default=True,blank=True,null=True)
-    categoria_id = models.ForeignKey(Categoria,blank=True,null=True,on_delete=models.SET_NULL)
+    categoria = models.ForeignKey(Categoria,blank=True,null=True,on_delete=models.SET_NULL)
+
+class Prestador(User):
+    telefono = models.CharField(max_length=15,blank=True,null=True)
+    activo = models.BooleanField(default=True,blank=True,null=True)
+    libro = models.ManyToManyField(Libro, through="DetallePrestamo")
+    def __str__(self):
+        return str(self.id)+" - "+self.username
+    def name(self):
+        return str(self.username)
+    def x(self):
+        return str(self.username+"-"+id)
 
 class Portada(models.Model):
     url = models.CharField(max_length=1000,blank=True,null=True)
     activo = models.BooleanField(default=True,blank=True,null=True)
-    libro_id = models.ForeignKey(Libro,blank=True,null=True,on_delete=models.SET_NULL)
+    libro = models.OneToOneField(Libro,blank=True,null=True,on_delete=models.SET_NULL)
 
 class Editorial(models.Model):
     nombre = models.CharField(max_length=100,blank=True,null=True)
@@ -41,10 +46,10 @@ class Editorial(models.Model):
     telefono = models.CharField(max_length=15,blank=True,null=True)
     correo = models.CharField(max_length=50,blank=True,null=True)
     activo = models.BooleanField(default=True,blank=True,null=True)
-    libro_id = models.ForeignKey(Libro,blank=True,null=True,on_delete=models.SET_NULL)
+    libro = models.ForeignKey(Libro,blank=True,null=True,on_delete=models.SET_NULL)
 class DetallePrestamo(models.Model):
     fecha_inicio = models.CharField(max_length=100,blank=True,null=True)
     fecha_vencimiento = models.CharField(max_length=100,blank=True,null=True)
     activo = models.BooleanField(default=True,blank=True,null=True)
-    prestador_id = models.ForeignKey(Prestador,blank=True,null=True,on_delete=models.SET_NULL)
-    libro_id = models.ForeignKey(Libro,blank=True,null=True,on_delete=models.SET_NULL)
+    prestador = models.ForeignKey(Prestador,blank=True,null=True,on_delete=models.SET_NULL)
+    libro = models.ForeignKey(Libro,blank=True,null=True,on_delete=models.SET_NULL)
